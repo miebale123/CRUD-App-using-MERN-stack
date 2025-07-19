12f7fb9c0411d4fe2bcd7167af61e8bfa35b5660
miebale123 <miebalen@gmail.com>
2025-07-19 11:41:14 +0300

    Fix frontend folder structure - add all frontend files properly

---
+++ front/src/Tiwaf.jsx
@@ -0,0 +1,20 @@
+export default function Tiwaf() {
+    return (
+        <div className="relative min-h-screen bg-gray-100">
+            hello
+            <div className="absolute inset-0 z-50 bg-black/60  flex items-center justify-center ">
+                <div className="holder">
+                    <div className="candle">
+                        <div className="blinking-glow"></div>
+                        <div className="thread"></div>
+                        <div className="glow"></div>
+                        <div className="flame"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+
